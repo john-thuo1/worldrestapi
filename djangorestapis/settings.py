00fe0e95a070276de80d44dc0e0995c7e16b57e6
john-thuo1 <108690517+john-thuo1@users.noreply.github.com>
2022-07-06 15:09:34 +0300

    Update settings.py

--- djangorestapis/settings.py
+++ djangorestapis/settings.py
@@ -11,26 +11,21 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k&cze$mbdct+_i1n@g*6@d+jxr1@)^m6@o4u8)wk=yn(r6wb(w'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'countries',
     'users',
     'rest_framework',
